7d02d889196cfdc0db7d9787ff085307b89c813c
Denice van den Steen <vandensteendenice@gmail.com>
2022-04-03 20:13:02 +0200

    Add menu basics

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,11 +4,14 @@ import "../Style/Components/Header.scss"
 export default function Home() {
   return (
     <div className="header-body">
-      <div className="header-title">
+      <Link to="/" className="header-title">
         <h1>Enicy</h1>
-      </div>
-      <div>
-        <Link to="/">Home</Link>
+      </Link>
+      <div className="header-links">
+        <Link className="link" to="/women">Dames</Link>
+        <Link className="link" to="/men">Heren</Link>
+        <Link className="link" to="/kids">Kinderen</Link>
+        <Link className="link" to="/sale">Sale</Link>
       </div>
     </div>
   );
